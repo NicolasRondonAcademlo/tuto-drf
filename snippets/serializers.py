bc7bd71407f016e49dcb5ad2a3a2c5366c729673
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-01-12 20:49:23 -0500

    tuto drf

---
+++ snippets/serializers.py
@@ -0,0 +1,31 @@
+from dataclasses import fields
+from re import S
+from rest_framework import serializers
+from .models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
+
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=False, max_length=100)
+#     code = serializers.CharField(style={"base_template": 'textarea.html'})
+#     lineos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
+
+#     def create(sefl, validate_data):
+#         return Snippet.objects.create(**validate_data)
+
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.lineos = validated_data.get('lineos', instance.lineos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        # fields = ('__all__')
+        fields = ['id', 'title', 'code', 'lineos', 'style']
